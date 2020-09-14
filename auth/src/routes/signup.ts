9ec8bf0241ed0d3a8654852921af0d4e799ff1d7
Nelson M <mwinel@live.com>
2020-09-14 20:42:12 +0300

    Add user signup validation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,20 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-  res.send("Hi there. This is the sign up route.");
-});
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Enter a valid email."),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Enter a strong password."),
+  ],
+  (req: Request, res: Response) => {
+    const { email, password } = req.body;
+  }
+);
 
 export { router as signUpRouter };
