6478425b4893cf24f576207afc58593f3a3ee499
Nelson M <mwinel@live.com>
2020-09-15 06:45:31 +0300

    Add error response normalization

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import 'express-async-errors'
 import { json } from "body-parser";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signUpRouter } from "./routes/signup";
 import { signInRouter } from "./routes/signin";
 import { signOutRouter } from "./routes/signout";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -13,6 +16,12 @@ app.use(currentUserRouter);
 app.use(signUpRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
+
+app.all("*", async (res, req) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000.");
