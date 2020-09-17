cfdaa71150cff6a63e25003b25950a86b6a40ec5
Nelson M <mwinel@live.com>
2020-09-17 21:07:21 +0300

    Add test auth helper function

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,15 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("returns details of a current user.", async () => {
-  const signUpResponse = await request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "test@test.com",
-      password: "password",
-    })
-    .expect(201);
-
-  const cookie = signUpResponse.get("Set-Cookie");
+  const cookie = await global.signin();
 
   const response = await request(app)
     .get("/api/users/currentuser")
@@ -18,5 +10,5 @@ it("returns details of a current user.", async () => {
     .send()
     .expect(200);
 
-  expect(response.body.currentUser.email).toEqual('test@test.com')
+  expect(response.body.currentUser.email).toEqual("test@test.com");
 });
