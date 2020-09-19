9386040dbdb98e04afcedeff6b5ea41cbf172819
Nelson M <mwinel@live.com>
2020-09-19 11:29:02 +0300

    Add routing to landing page on successful signup

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,18 @@
-function HomePage() {
-  return <h1>Welcome to Next.js!</h1>;
-}
+import buildClient from "../api/build-client";
 
-export default HomePage;
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
+};
+
+LandingPage.getInitialProps = async (context) => {
+  console.log("LANDING PAGE!");
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+  return data;
+};
+
+export default LandingPage;
