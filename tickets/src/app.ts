7414b9e211065d13b0dc52b7cb044cb04b265dd9
Nelson M <mwinel@live.com>
2020-09-22 18:41:20 +0300

    Add create ticker router

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,8 @@ import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError } from "@mwineltickets/common";
 
+import { createTicketRouter } from "./routes/new";
+
 const app = express();
 app.set("trust proxy", true);
 app.use(json());
@@ -13,6 +15,8 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
+
+app.use(createTicketRouter);
 
 app.all("*", async (res, req) => {
   throw new NotFoundError();
---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,14 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("returns a 201 on successful ticket creation with valid inputs.", async () => {
+  const response = await request(app).post("/api/tickets").send({});
+
+  expect(response.status).not.toEqual(404);
+});
+
+it("can only be accessed if the user is signed in.", async () => {});
+
+it("returns an error if an invalid title is provided.", async () => {});
+
+it("returns an error if an invalid price is provided.", async () => {});
