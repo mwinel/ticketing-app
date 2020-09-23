5829fa9fb1a81994328321b56f70a265c26fc317
Nelson M <mwinel@live.com>
2020-09-23 18:21:53 +0300

    Add title and price validation

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,20 @@
 import express, { Request, Response } from "express";
-import { requireAuth } from "@mwineltickets/common";
+import { body } from "express-validator";
+import { requireAuth, validateRequest } from "@mwineltickets/common";
 
 const router = express.Router();
 
-router.post("/api/tickets", requireAuth, (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.post(
+  "/api/tickets",
+  requireAuth,
+  [
+    body("title").not().isEmpty().withMessage("Title is required."),
+    body("price").isFloat({ gt: 0 }).withMessage("Enter a valid price."),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.sendStatus(200);
+  }
+);
 
 export { router as createTicketRouter };
